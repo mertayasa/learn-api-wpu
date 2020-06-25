86fc0c14a77af0611153edc363b45e09eb0b6da7
mertayasa <made.b4li@gmail.com>
2020-06-25 21:16:22 +0800

    Working with json in php and jquery

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$.getJSON('json/data/pizza.json', function(data){
+  let menu = data.menu;
+  console.log(menu)
+
+  $.each(menu, function(i, data){
+    $('#daftar-menu').append('<div class="col-3"><div class="card mb-3" style="width: 100%;"><img src="json/img/menu/'+ data.gambar +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">Rp. '+ data.harga +',-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+  });
+});
